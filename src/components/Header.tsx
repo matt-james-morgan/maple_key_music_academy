cc8182d93f9674af3507be792ef612f1b3a99c8c
Matt Morgan <matt.james.morgan97@gmail.com>
2025-08-17 10:58:55 -0400

    ham menu update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -58,7 +58,10 @@ const Header = () => {
           <button
             onClick={toggleMenu}
             className="md:hidden p-2 rounded-md text-gray-700 hover:text-red-600 hover:bg-gray-100"
-          ></button>
+          >
+            {" "}
+            <HamburgerIcon className="h-8 w-8 text-[#415d35]" />
+          </button>
         </div>
 
         {/* Mobile Navigation */}
@@ -104,3 +107,20 @@ const Header = () => {
 };
 
 export default Header;
+
+const HamburgerIcon = ({ className }: { className?: string }) => (
+  <svg
+    className={className}
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+  >
+    <line x1="3" y1="6" x2="21" y2="6" />
+    <line x1="3" y1="12" x2="21" y2="12" />
+    <line x1="3" y1="18" x2="21" y2="18" />
+  </svg>
+);
